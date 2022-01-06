b2c7b29e81ff6c72e4cf2c99f7526dabd6130e0c
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-01-06 11:43:40 +0700

    Basic Syntax 🟢

---
+++ 02-basic-syntax/index.php
@@ -0,0 +1,46 @@
+<?php
+// Basic Syntax
+
+// Standard Output
+//  echo, print()
+//  print_r()
+//  var_dump()
+
+// Penulisan Sintaks PHP
+//  1. PHP di dalam HTML
+//  2. HTML di dalam PHP
+
+// Variable dan Tipe Data
+//  tidak boleh diawali dengan angka
+//  $(name)
+
+// Operator
+//  aritmetika
+//  + - * / % 
+//  concatenation, untuk string
+//  .
+// Assignment
+//  = *= -= += /= %= .=
+
+
+
+
+
+$name = "DikDns";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Syntax</title>
+</head>
+<body>
+
+    <h1>Halo, Selamat Datang <?php echo $name ?></h1>
+    
+</body>
+</html>
